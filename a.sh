80d6a94ccc96bc158c464c5f407490baeb39b506
driverdrift <kxktb@outlook.com>
2026-01-27 17:41:09 +0800

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,11 @@
+DEVICE="/dev/sdc"
+
+echo -e "g\n					\
+	n\n\n\n+1M\nt\n4\n			\
+	n\n\n\n+100M\nt\n\nuefi\n	\
+	n\n\n\n+512M\n				\
+	n\n\n\n-2049M\n				\
+	n\n\n\n+1G\n				\
+	n\n\n\n\nt\n\nswap\n		\
+	w\n"
+
