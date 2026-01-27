c695caba9243b11b2fb00929dad7ab3bc3847962
driverdrift <kxktb@outlook.com>
2026-01-27 14:58:47 +0800

    Create choose_install_disk.sh

---
+++ choose_install_disk.sh
@@ -0,0 +1,33 @@
+choose_install_disk() {
+	local skip_confirm=${1:-false}
+	
+	# find rescue-os disk
+	sysdisk=$(lsblk -rno NAME,TYPE -s "$(findmnt -n -o SOURCE /)" | awk '$2=="disk"{print $1}')
+	# find original system disk which are going to install pure os.
+	mapfile -t disks < <(lsblk -dn -o NAME | grep -Ev "^($sysdisk|sr[0-9]+)$")
+	
+	case ${#disks[@]} in
+		0)
+			echo "There is no additional disk detected to install os"
+			exit 1
+			;;
+		1)
+			target="/dev/${disks[0]}"
+			echo "select $target automatically"
+			;;
+		*)
+			echo "There are many disks, please select the disk you want to install os"
+			select d in "${disks[@]}"; do
+				[ -n "$d" ] && { target="/dev/$d"; break; }
+			done
+			;;
+	esac
+	
+	if [ "$skip_confirm" = false ]; then
+		read -rp "Are you sure to foramt $target ? [No by default] (yes/y to confirm): " ans
+		ans=$(printf '%s' "$ans" | xargs)
+		[[ "$ans" =~ ^([Yy]|[Yy][Ee][Ss])$ ]] || exit 1
+	fi
+	
+	echo "$target"
+}
