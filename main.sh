6e458588c28b770c2b45ee731a43d9ada1a0f5ff
driverdrift <kxktb@outlook.com>
2026-01-29 19:31:34 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,11 +8,15 @@ if [[ "${1-}" =~ ^([Yy][Ee][Ss]|[Yy])$ ]]; then
     skip_confirm=true
 fi
 
+echo "Start disk partiton."
 source ./disk_partition.sh
 apt-get update 1>/dev/null
 disk_to_install=$(disk_partition "$skip_confirm")
+echo "Disk partiton completed."
 
+echo "Start making install media."
 source ./make_install_media.sh
 $(make_install_media "$disk_to_install")
-sleep 15
-poweroff
+echo "Making install media completed."
+
+echo -e "All things completed. You can now exit the rescue mode, \nand then poweron your original os to continue installation..."
